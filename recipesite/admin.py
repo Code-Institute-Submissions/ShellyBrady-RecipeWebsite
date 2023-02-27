74c8691a010c75a47d133886de8b38d5c252a25c
ShellyBrady <mchellebr@hotmail.com>
2023-02-27 10:52:03 +0000

    add log in settings to settings.py and create superuser for admin

--- recipesite/admin.py
+++ recipesite/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
+#same as i think therefore i blog walkthrough
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
@@ -10,7 +11,7 @@ class PostAdmin(SummernoteModelAdmin):
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
     list_filter = ('status', 'created_on')
-    summernote_fields = ('content')
+    summernote_fields = ('content',)
 
 
 @admin.register(Comment)
